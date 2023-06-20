2b8f83b22a8cad816b2cf00ae293bffa6d0f3455
Emily Koceja <117419130+eskoceja@users.noreply.github.com>
2023-06-20 10:36:29 -0400

    Delete JDBCLab directory

--- JDBCLab/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        DatabaseConnector connector = new DatabaseConnector();
-        connector.connect();
-//        connector.selectAllBooks();
-    }
-}
